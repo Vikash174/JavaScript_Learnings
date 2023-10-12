9df8763bde7a22e45b40c1216500d1056f4d2134
Vikash rai <71267021+Vikash174@users.noreply.github.com>
2023-10-13 00:32:32 +0530

    Learn OOPS Concepts

---
+++ Constructor_Funtions_&_Prototypes/app.js
@@ -0,0 +1,38 @@
+// class AgedPerson{
+//   printAge(){
+//     console.log(this.age);
+//   }   
+// }
+
+
+// class Person extends AgedPerson{
+//     name = 'Max';
+//     constructor(){
+//         super();
+//         this.age = 30;
+//     }
+
+//     greet(){
+//         console.log(`Hi my name is ${this.name} and I am ${this.age} years old`);
+//     } 
+// } 
+
+// /* function Person(){
+//     this.age = 30;
+//     this.name = 'Max';
+//     this.greet = function(){
+//         console.log(`Hi my name is ${this.name} and I am ${this.age} years old`);
+//     };
+// } */
+
+
+//  console.dir(Person);
+
+// const p = new Person();
+// p.greet();
+// p.printAge();
+// console.log(p.__proto__);
+
+function adminUser(userName){
+    this.name = userName;
+}
